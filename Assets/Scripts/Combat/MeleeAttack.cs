c39f6618fe35d155c057469055e27395eebffa17
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-01-25 16:26:19 -0500

    set up some basic health functoins

--- Assets/Scripts/Combat/MeleeAttack.cs
+++ Assets/Scripts/Combat/MeleeAttack.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MeleeAttack : MonoBehaviour
 {
     public BoxCollider2D range;
-    private Transform enemy;
+    public Transform enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +21,7 @@ public class MeleeAttack : MonoBehaviour
     public void Attack()
     {
         range.edgeRadius = 2;
+
         if (Vector2.Distance(enemy.position, transform.position) <= range.edgeRadius) 
         {
            Debug.Log("Enemy was attacked");
--- Assets/Scripts/Combat/TragetEnemy.cs
+++ Assets/Scripts/Combat/TragetEnemy.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TragetEnemy : MonoBehaviour
 { 
     public Camera camera;
+    private Rigidbody2D rb;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
