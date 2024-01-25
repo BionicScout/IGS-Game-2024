092d6501af3f0859e2e4ce0a15ede598df1794c5
BionicScout <90854007+BionicScout@users.noreply.github.com>
2024-01-25 15:12:41 -0500

    Centered Grid (I think)

--- Assets/Scripts/Hex Map/HexLayout.cs
+++ Assets/Scripts/Hex Map/HexLayout.cs
@@ -5,89 +5,6 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class HexLayout : MonoBehaviour {
-
-    //struct Orientation {
-    //    public double f0, f1, f2, f3;
-    //    public double b0, b1, b2, b3;
-    //    public double start_angle; // in multiples of 60°
-    //    public Orientation(double f0_ , double f1_ , double f2_ , double f3_ ,
-    //                        double b0_ , double b1_ , double b2_ , double b3_ ,
-    //                        double start_angle_) {
-    //        f0 = f0_;
-    //        f1 = f1_;
-    //        f2 = f2_;
-    //        f3 = f3_;
-    //        b0 = b0_;
-    //        b1 = b1_;
-    //        b2 = b2_;
-    //        b3 = b3_;
-    //        start_angle = start_angle_;
-    //    }
-
-    //};
-
-    //Orientation layout_pointy =
-    //    new Orientation(Mathf.Sqrt(3.0f) , Mathf.Sqrt(3.0f) / 2.0 , 0.0 , 3.0 / 2.0 ,
-    //                    Mathf.Sqrt(3.0f) / 3.0 , -1.0 / 3.0 , 0.0 , 2.0 / 3.0 ,
-    //                    0.5);
-
-    //struct Layout {
-    //    public Orientation orientation;
-    //    public Point size;
-    //    public Point origin;
-    //    public Layout(Orientation orientation_ , Point size_ , Point origin_) {
-    //        orientation = orientation_;
-    //        size = size_;
-    //        origin = origin_;
-    //    }
-    //};
-
-    //struct Point {
-    //    public double x, y;
-    //    public Point(double x_ , double y_) {
-    //        x = x_;
-    //        y = y_;
-    //    }
-    //};
-
-    //Point hex_to_pixel(Layout layout , Hex h) {
-    //    Orientation op = layout.orientation;
-    //    double x = (op.f0 * h.q + op.f1 * h.r) * layout.size.x;
-    //    double y = (op.f2 * h.q + op.f3 * h.r) * layout.size.y;
-    //    return new Point(x + layout.origin.x , y + layout.origin.y);
-    //}
-
-    ////public FractionalHex pixel_to_hex(Layout layout , Point p) {
-    ////{
-    ////    const Orientation op = layout.orientation;
-    ////    Point pt = Point((p.x - layout.origin.x) / layout.size.x ,
-    ////                        (p.y - layout.origin.y) / layout.size.y);
-    ////    double q = op.b0 * pt.x + op.b1 * pt.y;
-    ////    double r = op.b2 * pt.x + op.b3 * pt.y;
-    ////    return FractionalHex(q , r , -q - r);
-    ////}
-
-
-
-    //Point hex_corner_offset(Layout layout , int corner) {
-    //    Point size = layout.size;
-    //    float angle = (float)(2.0 * Mathf.PI * (layout.orientation.start_angle + corner) / 6);
-    //    return new Point(size.x * Mathf.Cos(angle) , size.y * Mathf.Sin(angle));
-    //}
-
-    //List<Point> polygon_corners(Layout layout , Hex h) {
-    //    List<Point> corners = new List<Point>{ };
-    //    Point center = hex_to_pixel(layout , h);
-
-    //    for(int i = 0; i < 6; i++) {
-    //        Point offset = hex_corner_offset(layout , i);
-    //        corners.Add(new Point(center.x + offset.x , center.y + offset.y));
-    //    }
-    //    return corners;
-    //}
-
-
-
     public GameObject Testagon;
     public Vector2Int size;
 
@@ -106,29 +23,10 @@ public class HexLayout : MonoBehaviour {
         ImportTest();
     }
 
-    int GetBottomRowY(Tilemap tilemap) {
-        BoundsInt bounds = tilemap.cellBounds;
-        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
-
-        int minRowY = int.MaxValue;
-
-        foreach(Vector3Int pos in bounds.allPositionsWithin) {
-            if(tilemap.HasTile(pos)) {
-                minRowY = Mathf.Min(minRowY , pos.y);
-            }
-        }
-
-        return minRowY;
-    }
-
     public void ImportTest() {
         Tilemap tilemap = tilemapObj.transform.GetChild(0).GetComponent<Tilemap>();
-        BoundsInt bounds = tilemap.cellBounds;
         tilemap.CompressBounds();
-
-        int bottomRowY = GetBottomRowY(tilemap);
-        Debug.Log("Y coordinate of bottom row: " + bottomRowY);
-
+        BoundsInt bounds = tilemap.cellBounds;
         TilemapRenderer tilemapRenderer = tilemapObj.transform.GetChild(0).GetComponent<TilemapRenderer>();
 
 
@@ -144,7 +42,7 @@ public class HexLayout : MonoBehaviour {
                 TileBase tile = allTiles[x + y * bounds.size.x];
                 if(tile != null) {
                     Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
-                    GameObject tileObj = CreateHexTile(x , y , tile.name, bottomRowY % 2);
+                    GameObject tileObj = CreateHexTile(x , y , tile.name);
                     tileObj.transform.parent = header.transform;
                 }
                 else {
@@ -155,7 +53,7 @@ public class HexLayout : MonoBehaviour {
 
         Destroy(tilemapObj);
 
-        /*
+
         //Adjust Center Postiton
         Vector3Int max = new Vector3Int();
         Vector3Int min = new Vector3Int();
@@ -171,7 +69,7 @@ public class HexLayout : MonoBehaviour {
                 min.y = t.Key.y;
         }
 
-        Vector3Int centerTile = UnityCoords_To_CubicCoords(bounds.size.y / 2 , bounds.size.x / 2, bottomRowY % 2);
+        Vector3Int centerTile = UnityCoords_To_CubicCoords(bounds.size.y / 2 , bounds.size.x / 2);
         //Vector3Int centerTile = (max + min) / 2;
         //centerTile.z = -centerTile.x - centerTile.y;
         Vector3 centerPos = GlobalVars.hexagonTile[centerTile].transform.position;
@@ -180,17 +78,20 @@ public class HexLayout : MonoBehaviour {
 
         foreach(var t in GlobalVars.hexagonTile) {
             t.Value.transform.position -= centerPos;
-        }*/
+        }
+
+        //Set Move
+        //GlobalVars.hexagonTile[centerTile].transform.position
     }
 
-    private Vector3Int UnityCoords_To_CubicCoords(int y , int x, int evenGrid) {
+    private Vector3Int UnityCoords_To_CubicCoords(int y , int x) {
         var q = y - (x + 1) / 2; //var q = y - (x + (evenGrid & 1)) / 2;
         var r = x;
         return new Vector3Int(q , r , -q - r);
     }
 
-    public GameObject CreateHexTile(int x, int y, string spriteName, int evenGrid) {
-        Hex h = new Hex(UnityCoords_To_CubicCoords(x, y, evenGrid));
+    public GameObject CreateHexTile(int x, int y, string spriteName) {
+        Hex h = new Hex(UnityCoords_To_CubicCoords(x, y));
         GlobalVars.availableHexes.Add(new Vector3Int(h.q , h.r , h.s));
 
         GameObject obj = Instantiate(Testagon);
@@ -202,7 +103,7 @@ public class HexLayout : MonoBehaviour {
         obj.transform.position = pos + offset;
 
         obj.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = "(" + x.ToString() + ", " + y.ToString() + ")";
-        obj.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = "";
+        //obj.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = "";
         //obj.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = spriteName;
 
         GlobalVars.hexagonTile.Add(new Vector3Int(h.q , h.r , h.s) , obj);
@@ -223,7 +124,7 @@ public class HexLayout : MonoBehaviour {
             }
         }
 
-        Debug.LogWarning("Tile Sprite Doesn't Exist - " + spriteName);
+        //Debug.LogWarning("Tile Sprite Doesn't Exist - " + spriteName);
         return null;
     }
 
