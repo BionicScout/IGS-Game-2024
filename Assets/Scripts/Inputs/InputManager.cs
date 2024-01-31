5ba32b7bdd0ed8714eb691c4a343c5d5c5202d69
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-01-31 12:14:15 -0500

    redid click detection

--- Assets/Scripts/Inputs/InputManager.cs
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
+using System.Collections;
 
-public class InputManager : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
+public class InputManager : MonoBehaviour
+{
+    void Update()
+    {
+        GetPosition();
+    }
+    public void GetPosition()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.transform.position);
+                return hit.collider.gameObject.transform.position;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update() {
-
-    }
 }
+
