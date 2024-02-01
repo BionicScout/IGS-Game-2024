c0e6ea1bc8cc5368c5f9450f4c678277ed08565a
BionicScout <90854007+BionicScout@users.noreply.github.com>
2024-02-01 12:43:52 -0500

    HEre Take THis

--- Assets/Scripts/Combat/Loading/CharacterLoader.cs
+++ Assets/Scripts/Combat/Loading/CharacterLoader.cs
@@ -11,6 +11,15 @@ public class CharacterLoader : MonoBehaviour {
 
     private void Start() {
         GlobalVars.players.Add(GlobalVars.centerHex, players[0]);
-        GlobalVars.enemies.Add(GlobalVars.centerHex , enemies[0]);
+        GlobalVars.enemies.Add(GlobalVars.centerHex + 2 * Hex.hex_directions[0] , enemies[0]);
+
+        GameObject currentTileObj = GlobalVars.hexagonTile[GlobalVars.centerHex];
+        GameObject newTileObj = GlobalVars.hexagonTile[GlobalVars.centerHex + 2 * Hex.hex_directions[0]];
+
+        //Update Sprite
+        currentTileObj.transform.GetChild(2).GetComponent<SpriteRenderer>().sprite = players[0].sprite;
+        newTileObj.transform.GetChild(2).GetComponent<SpriteRenderer>().sprite = enemies[0].sprite;
+
+
     }
 }
