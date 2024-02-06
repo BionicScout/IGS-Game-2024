f72f610d27cf8e1b0e25016f480b141804a5b3c5
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-02-06 16:24:36 -0500

    camera bounds are working

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
-{
+{    
+    [SerializeField]
+    private Camera cam;
+
     private float zoom;
     private float zoomStep = 4f;
     private float minZoom = 2f;
@@ -11,13 +14,22 @@ public class CameraMove : MonoBehaviour
     private float velocity = 0f;
     private float smoothTime = 0.25f;
 
+    [SerializeField]
+    public SpriteRenderer spriteRenderer;
+
     private float mapMinX;
     private float mapMinY;
     private float mapMaxX;
     private float mapMaxY;
 
-    [SerializeField]
-    private Camera cam;
+    private void Awake()
+    {
+        mapMinX = spriteRenderer.transform.position.x - spriteRenderer.bounds.size.x / 2f;
+        mapMaxX = spriteRenderer.transform.position.x + spriteRenderer.bounds.size.x / 2f;
+
+        mapMinY = spriteRenderer.transform.position.y - spriteRenderer.bounds.size.y / 2f;
+        mapMaxY = spriteRenderer.transform.position.y + spriteRenderer.bounds.size.y / 2f;
+    }
 
     private Vector3 dragOrigin;
 
@@ -26,18 +38,18 @@ public class CameraMove : MonoBehaviour
         zoom = cam.orthographicSize;
 
         ////gets bottom and left side of map
-        Vector3 bottomLeft = GlobalVars.hexagonTile[new Vector3Int(0, 0, 0)].transform.position;
-        mapMinX = bottomLeft.x;
-        mapMinY = bottomLeft.y;
+        //Vector3 bottomLeft = GlobalVars.hexagonTile[new Vector3Int(0, 0, 0)].transform.position;
+        //mapMinX = bottomLeft.x;
+        //mapMinY = bottomLeft.y;
 
-        //gets top and right side of map
-        Vector3 TopRight = GlobalVars.hexagonTile[GlobalVars.topRightHex].transform.position;
-        mapMaxX = TopRight.x;
-        mapMaxY = TopRight.y;
+        ////gets top and right side of map
+        //Vector3 TopRight = GlobalVars.hexagonTile[GlobalVars.topRightHex].transform.position;
+        //mapMaxX = TopRight.x;
+        //mapMaxY = TopRight.y;
 
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         //zooms in and out
         float scroll = Input.GetAxis("Mouse ScrollWheel");
@@ -49,17 +61,17 @@ public class CameraMove : MonoBehaviour
 
         //checks the bounds and wont let the camera pass them
         //use Mathf.clamp
-        float xBounds = +cam.aspect / 2f;
-        if (cam.transform.position.x < xBounds)
-        {
-            float cameraPos = xBounds;
-        }
+        //float xBounds = +cam.aspect / 2f;
+        //if (cam.transform.position.x < xBounds)
+        //{
+        //    float cameraPos = xBounds;
+        //}
 
-        float yBounds = +cam.orthographicSize / 2f;
-        if (cam.transform.position.y < yBounds)
-        {
-            float cameraPos = yBounds;
-        }
+        //float yBounds = +cam.orthographicSize / 2f;
+        //if (cam.transform.position.y < yBounds)
+        //{
+        //    float cameraPos = yBounds;
+        //}
 
         PanCamera();
     }
@@ -75,6 +87,23 @@ public class CameraMove : MonoBehaviour
         {
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
             cam.transform.position += difference;
+            cam.transform.position = ClampCam(cam.transform.position + difference);
         }
+
+    }
+    private Vector3 ClampCam(Vector3 targetPos)
+    {
+        float camHeight = cam.orthographicSize;
+        float camWidth = cam.orthographicSize * cam.aspect;
+
+        float minX = mapMinX + camWidth;
+        float maxX = mapMaxX - camWidth;
+        float minY = mapMinY + camHeight;
+        float maxY = mapMaxY - camHeight;
+
+        float newX = Mathf.Clamp(targetPos.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPos.y, minY, maxY);
+
+        return new Vector3(newX, newY, targetPos.z);
     }
 }
--- Assets/Scripts/Combat/Combatents/Player/LevelingUp.cs
+++ Assets/Scripts/Combat/Combatents/Player/LevelingUp.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class LevelingUp : MonoBehaviour
 {
-    //
+    private int move = 4;
+    private int maxHealth = 10;
+    private float dodgeChance;
+    private int defense;
+    private int power; 
+
+
+    public void TurnipLvl()
+    {
+        foreach (KeyValuePair<Vector3Int, Stats> info in GlobalVars.players)
+        {
+            
+        }
+    }
+
+    public void ParsnipLvl()
+    {
+        foreach (KeyValuePair<Vector3Int, Stats> info in GlobalVars.players)
+        {
+            Stats move;
+        }
+    }
+
+    public void BeetLvl()
+    {
+        foreach (KeyValuePair<Vector3Int, Stats> info in GlobalVars.players)
+        {
+            Stats move;
+        }
+    }
 }
+
