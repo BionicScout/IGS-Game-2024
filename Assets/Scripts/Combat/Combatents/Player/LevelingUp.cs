d234c19901e4975c9993cf5fe2f98d4db773c499
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-02-08 16:42:29 -0500

    Lvling might now work for each diff character

--- Assets/Scripts/Combat/Combatents/Player/LevelingUp.cs
+++ Assets/Scripts/Combat/Combatents/Player/LevelingUp.cs
@@ -4,35 +4,31 @@ using UnityEngine;
 
 public class LevelingUp : MonoBehaviour
 {
-    private int move = 4;
-    private int maxHealth = 10;
-    private float dodgeChance;
-    private int defense;
-    private int power; 
-
 
     public void TurnipLvl()
     {
         foreach (KeyValuePair<Vector3Int, Stats> info in GlobalVars.players)
         {
-            //gets each var in stats increases by one then updates the dictionary
-            Debug.Log("Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
-            GlobalVars.players[new Vector3Int(0, 0 , 0)].move += 1;
-            Debug.Log("New Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+            //can check their character name
+            if(GlobalVars.players[new Vector3Int(0, 0, 0)].charName == "Turnip")
+            {
+                //gets each var in stats increases by one then updates the dictionary
+                Debug.Log("Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].move += 1;
+                Debug.Log("New Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
 
-            Debug.Log("Power: " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
-            GlobalVars.players[new Vector3Int(0, 0, 0)].power += 1;
-            Debug.Log("New Power " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+                Debug.Log("Power: " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].power += 1;
+                Debug.Log("New Power " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
 
-            Debug.Log("Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
-            GlobalVars.players[new Vector3Int(0, 0, 0)].defense += 1;
-            Debug.Log("New Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+                Debug.Log("Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].defense += 1;
+                Debug.Log("New Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
 
-            Debug.Log("Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
-            GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth += 1;
-            Debug.Log("New Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
-
-
+                Debug.Log("Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth += 1;
+                Debug.Log("New Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+            }
             //how lists and dictionarys kinda aer called
             //List<Stats> list = new List<Stats>();
             //list.Add(new Stats());
@@ -41,9 +37,6 @@ public class LevelingUp : MonoBehaviour
             //Dictionary<Vector3Int, Stats> dict = new Dictionary<Vector3Int, Stats>();
             //dict.Add(new Vector3Int(1, -1, 0), new Stats());
             //dict[new Vector3Int(1, -1, 0)];
-
-
-
         }
     }
 
@@ -51,7 +44,25 @@ public class LevelingUp : MonoBehaviour
     {
         foreach (KeyValuePair<Vector3Int, Stats> info in GlobalVars.players)
         {
-            Stats move;
+            if (GlobalVars.players[new Vector3Int(0, 0, 0)].charName == "Carrot")
+            {
+                //gets each var in stats increases by one then updates the dictionary
+                Debug.Log("Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].move += 1;
+                Debug.Log("New Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+
+                Debug.Log("Power: " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].power += 1;
+                Debug.Log("New Power " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+
+                Debug.Log("Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].defense += 1;
+                Debug.Log("New Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+
+                Debug.Log("Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth += 1;
+                Debug.Log("New Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+            }
         }
     }
 
@@ -59,7 +70,25 @@ public class LevelingUp : MonoBehaviour
     {
         foreach (KeyValuePair<Vector3Int, Stats> info in GlobalVars.players)
         {
-            Stats move;
+            if (GlobalVars.players[new Vector3Int(0, 0, 0)].charName == "Beet")
+            {
+                //gets each var in stats increases by one then updates the dictionary
+                Debug.Log("Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].move += 1;
+                Debug.Log("New Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+
+                Debug.Log("Power: " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].power += 1;
+                Debug.Log("New Power " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+
+                Debug.Log("Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].defense += 1;
+                Debug.Log("New Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+
+                Debug.Log("Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+                GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth += 1;
+                Debug.Log("New Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+            }
         }
     }
 }
