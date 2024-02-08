2733b88120d998adf421f8a207e255efc08b3f5d
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-02-08 13:04:10 -0500

    Leveling up is kinda working

--- Assets/Scripts/Combat/Combatents/Player/LevelingUp.cs
+++ Assets/Scripts/Combat/Combatents/Player/LevelingUp.cs
@@ -15,7 +15,35 @@ public class LevelingUp : MonoBehaviour
     {
         foreach (KeyValuePair<Vector3Int, Stats> info in GlobalVars.players)
         {
-            
+            //gets each var in stats increases by one then updates the dictionary
+            Debug.Log("Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+            GlobalVars.players[new Vector3Int(0, 0 , 0)].move += 1;
+            Debug.Log("New Move: " + GlobalVars.players[new Vector3Int(0, 0, 0)].move);
+
+            Debug.Log("Power: " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+            GlobalVars.players[new Vector3Int(0, 0, 0)].power += 1;
+            Debug.Log("New Power " + GlobalVars.players[new Vector3Int(0, 0, 0)].power);
+
+            Debug.Log("Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+            GlobalVars.players[new Vector3Int(0, 0, 0)].defense += 1;
+            Debug.Log("New Defense: " + GlobalVars.players[new Vector3Int(0, 0, 0)].defense);
+
+            Debug.Log("Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+            GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth += 1;
+            Debug.Log("New Max Health: " + GlobalVars.players[new Vector3Int(0, 0, 0)].maxHealth);
+
+
+            //how lists and dictionarys kinda aer called
+            //List<Stats> list = new List<Stats>();
+            //list.Add(new Stats());
+            //list[0];
+
+            //Dictionary<Vector3Int, Stats> dict = new Dictionary<Vector3Int, Stats>();
+            //dict.Add(new Vector3Int(1, -1, 0), new Stats());
+            //dict[new Vector3Int(1, -1, 0)];
+
+
+
         }
     }
 
