9738a668d5d09837dacdf66733bc290bed5e8e46
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-02-15 16:40:46 -0500

    trying to smooth out cam movements

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
-{    
+{
     [SerializeField]
     private Camera cam;
 
@@ -37,60 +37,31 @@ public class CameraMove : MonoBehaviour
     {
         zoom = cam.orthographicSize;
 
-        ////gets bottom and left side of map
-        //Vector3 bottomLeft = GlobalVars.hexagonTile[new Vector3Int(0, 0, 0)].transform.position;
-        //mapMinX = bottomLeft.x;
-        //mapMinY = bottomLeft.y;
-
-        ////gets top and right side of map
-        //Vector3 TopRight = GlobalVars.hexagonTile[GlobalVars.topRightHex].transform.position;
-        //mapMaxX = TopRight.x;
-        //mapMaxY = TopRight.y;
-
     }
 
     private void LateUpdate()
     {
-        //zooms in and out
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        zoom -= scroll * zoomStep;
-        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
-        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
 
-
-
-        //checks the bounds and wont let the camera pass them
-        //use Mathf.clamp
-        //float xBounds = +cam.aspect / 2f;
-        //if (cam.transform.position.x < xBounds)
-        //{
-        //    float cameraPos = xBounds;
-        //}
-
-        //float yBounds = +cam.orthographicSize / 2f;
-        //if (cam.transform.position.y < yBounds)
-        //{
-        //    float cameraPos = yBounds;
-        //}
-
-        PanCamera();
-    }
-
-    private void PanCamera()
-    {
         //moves camera when right click is held and moved
         if (Input.GetMouseButtonDown(1))
         {
             dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
         }
-        if(Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))
         {
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
             cam.transform.position += difference;
             cam.transform.position = ClampCam(cam.transform.position + difference);
         }
 
+        //zooms in and out
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        zoom -= scroll * zoomStep;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+
     }
+
     private Vector3 ClampCam(Vector3 targetPos)
     {
         float camHeight = cam.orthographicSize;
