041fb13b682edc193cbb9908ae2e3cc5ee20dbcd
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-02-27 15:47:58 -0500

    stuff + charcter selector

---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[SerializeField]
+public class Characters
+{
+    public GameObject prefab;
+    public string name;
+    public Sprite sprite;
+}
