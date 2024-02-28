63b78e01761e4f106fd255118c9f782c97f79c94
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-02-28 01:43:43 -0500

    saves prev options

--- Assets/Scripts/Character Selection/CharacterManager.cs
+++ Assets/Scripts/Character Selection/CharacterManager.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharacterManager : MonoBehaviour
 {
@@ -15,6 +18,14 @@ public class CharacterManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (PlayerPrefs.HasKey("selectedOption"))
+        {
+            selectedOption = 0;
+        }
+        else
+        {
+            Load();
+        }
         UpdateCharacter(selectedOption);
     }
     public void NextOption()
@@ -25,6 +36,7 @@ public class CharacterManager : MonoBehaviour
             selectedOption = 0;
         }
         UpdateCharacter(selectedOption);
+        Save();
     }
     public void BackOption()
     {
@@ -34,11 +46,24 @@ public class CharacterManager : MonoBehaviour
             selectedOption = characterData.CharacterCount - 1;
         }
         UpdateCharacter(selectedOption);
+        Save();
     }
     private void UpdateCharacter(int selectedOption)
     {
         Characters character = characterData.GetCharacter(selectedOption);
         spriteIcon.sprite = character.charSprite;
         nameTxt.text = character.charName;
+    }
+    private void Load()
+    {
+        selectedOption = PlayerPrefs.GetInt("selectedOption");
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("selectedOption", selectedOption);
+    }
+    public void ChangeScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
     }
 }
