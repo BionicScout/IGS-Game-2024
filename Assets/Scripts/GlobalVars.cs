80c5139aaccf876417ae49d54adaa53d51cb3f3c
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-03-26 15:07:18 -0400

    did some quick changes

--- Assets/Scripts/GlobalVars.cs
+++ Assets/Scripts/GlobalVars.cs
@@ -5,8 +5,8 @@ public static class GlobalVars {
     //Map Info
     public static Dictionary<Vector3Int , GameObject> hexagonTile = new Dictionary<Vector3Int , GameObject>(); //Vector3Int - The Cubic Coord of Hex; Hex - The Hex Information
     public static Dictionary<Vector3Int , TileScriptableObjects> hexagonTileRefrence = new Dictionary<Vector3Int , TileScriptableObjects>(); //Vector3Int - The Cubic Coord of Hex; Hex - The Hex Information
-    public static Dictionary<Vector3Int, int> poisonTiles = new Dictionary<Vector3Int , int>(); //wills tore what tiles have poision and how many turns left
-    public static Dictionary<Vector3Int, int> smokeTiles = new Dictionary<Vector3Int, int>(); //wills tore what tiles have poision and how many turns left
+    public static Dictionary<Vector3Int, int> poisonTiles = new Dictionary<Vector3Int , int>(); //will store what tiles have poision and how many turns left
+    public static Dictionary<Vector3Int, int> smokeTiles = new Dictionary<Vector3Int, int>(); //will store what tiles have poision and how many turns left
     public static List<Vector3Int> availableHexes = new List<Vector3Int>(); //A list holding the coords of tile that exist on the map. 
 
     public static Vector3Int bottomLeftHex = new Vector3Int(0, 0 ,0);
@@ -20,7 +20,7 @@ public static class GlobalVars {
     //Combat Info
     public static Dictionary<Vector3Int , Stats> players = new Dictionary<Vector3Int , Stats>();
     public static Dictionary<Vector3Int , Stats> enemies = new Dictionary<Vector3Int , Stats>();
-
+    public static Dictionary<Vector3Int, Stats> deadPlayers = new Dictionary<Vector3Int, Stats>(); 
     public static Queue<SpawnWave> spawnWaves = new Queue<SpawnWave>();
 
     //Specific Level info
