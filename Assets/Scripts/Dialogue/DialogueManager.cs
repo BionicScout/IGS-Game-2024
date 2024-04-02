1cf6eddb3121109e51f596bc8f72391a96a121b7
KyleighS <111814352+KyleighS@users.noreply.github.com>
2024-04-02 15:13:39 -0400

    Dialogue system is working very well

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> sentences;
+    public GameObject dialogueCanvas;
+    public GameObject selectedPlayerMenu;
+    public TextMeshProUGUI nameTxt, dialogueTxt;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +19,41 @@ public class DialogueManager : MonoBehaviour
     }
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        animator.SetBool("IsOpen", true);
+        nameTxt.text = dialogue.name;
         sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueTxt.text = " ";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueTxt.text += letter;
+            yield return null;
+        }
+    }
+    public void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+        dialogueCanvas.SetActive(false);
+        selectedPlayerMenu.SetActive(true);
+        Debug.Log("End of conversation");
     }
 }
